0b57bf958db298ee60a5c831fff87b65af7c6a4f
katherine-p <c14180221@john.petra.ac.id>
2021-02-23 19:45:04 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 3;
+
+  item = [
+    {nama : "Jeruk", harga : 15000, satuan : "kg"},
+    {nama : "Apel", harga : 5000, satuan : "kg" }
+  ]
+
+  tampil : boolean = true;
+
+  tulisanopen : string;
+
+  open(){
+    this.tulisanopen = "Toko dibuka";
+    alert("dibukaa");
+  }
+
+  values = "";
+  onKey(event: any){
+    this.values += event.target.value + ' | ';
+
+  }
 }
